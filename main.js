143fe5c56b15f166bfab38673c18503540dea0ea
Guillermo Marincic <gjmlmarincic@gmail.com>
2023-07-24 18:12:03 -0300

    primer desafio

--- main.js
+++ main.js
@@ -1,93 +1,94 @@
 class Product {
     constructor(title, description, price, thumbnail, code, stock) {
-      this.title = title;
-      this.description = description;
-      this.price = price;
-      this.thumbnail = thumbnail;
-      this.code = code;
-      this.stock = stock;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
     }
-  }
-  
-  class ProductManager {
-    products = [];
-    productIdCounter = 1;
-  
+}
+
+class ProductManager {
     constructor() {
-      this.products = [];
-      this.productIdCounter = 1;
+        this.products = [];
+        this.productIdCounter = 1;
     }
-  
-    addProduct(product) {
-      if (this.products.find((p) => p.code === product.code)) {
-        throw new Error("Product code already exists");
-      }
-  
-      if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
-        throw new Error("All fields are required");
-      }
-  
-      // Asignar un id autoincrementable al producto
-      const productWithId = {
-        ...product,
-        id: this.productIdCounter,
-      };
-      this.productIdCounter++;
-  
-      this.products.push(productWithId);
+
+    addProduct(productData) {
+        // Validar que todos los campos sean obligatorios
+        if (
+            !productData.title || !productData.description || !productData.price || !productData.thumbnail || !productData.code || !productData.stock
+        ) {
+            console.log("Faltan campos obligatorios");
+            return;
+        }
+
+        // Validar que el campo "code" no se repita
+        const codigo = productData.code;
+        const productoExistente = this.products.find((producto) => producto.code === codigo);
+        if (productoExistente) {
+            console.log("El código del producto ya existe");
+            return;
+        }
+
+        // Agregar el producto al arreglo con un id autoincrementable
+        const productoConId = {
+            ...productData,
+            id: this.productIdCounter,
+        };
+        this.productIdCounter++;
+
+        this.products.push(productoConId);
     }
-  
+
     getProducts() {
-      return this.products;
+        return this.products;
     }
-  
+
     getProductById(id) {
-      const product = this.products.find((p) => p.id === id);
-  
-      if (!product) {
-        console.error("Product not found");
-        throw new Error("Product not found");
-      }
-  
-      return product;
+        const producto = this.products.find((producto) => producto.id === id);
+        if (!producto) {
+            console.error("No encontrado");
+            return null;
+        }
+        return producto;
     }
-  }
-  
-  // Ejemplo de uso con prompts:
-  
-  const productManager = new ProductManager();
-  
-  while (true) {
-    const title = prompt("Enter product title:");
-    const description = prompt("Enter product description:");
-    const price = parseFloat(prompt("Enter product price:"));
-    const thumbnail = prompt("Enter product thumbnail URL:");
-    const code = prompt("Enter product code:");
-    const stock = parseInt(prompt("Enter product stock:"));
-  
-    try {
-      const newProduct = new Product(title, description, price, thumbnail, code, stock);
-      productManager.addProduct(newProduct);
-      console.log("Product added successfully!");
-    } catch (error) {
-      console.error(error.message);
-    }
-  
-    const continueAdding = confirm("Do you want to add another product?");
-    if (!continueAdding) {
-      break;
-    }
-  }
-  
-  console.log("All products:");
-  console.log(productManager.getProducts());
-  
-  const searchById = parseInt(prompt("Enter product id to search:"));
-  try {
-    const product = productManager.getProductById(searchById);
-    console.log("Product found:");
-    console.log(product);
-  } catch (error) {
-    console.error(error.message);
-  }
-  
+}
+
+// Ejemplo de uso:
+
+const productManager = new ProductManager();
+
+productManager.addProduct({
+    title: "Potus",
+    description: "Variegado",
+    price: 10.99,
+    thumbnail: "imagen no disponible",
+    code: "p1",
+    stock: 100,
+});
+
+productManager.addProduct({
+    title: "Monstera Deliciosa",
+    description: "Cuidado Fácil",
+    price: 19.99,
+    thumbnail: "imagen no disponible",
+    code: "p2",
+    stock: 50,
+});
+
+productManager.addProduct({
+    title: "Flor de la paz",
+    description: "Cuidado Moderado",
+    price: 200,
+    thumbnail: "imagen no disponible",
+    code: "p3",
+    stock: 250,
+});
+
+console.log(productManager.getProducts());
+console.log(productManager.getProductById(1));
+console.log(productManager.getProductById(2));
+console.log(productManager.getProductById(3)); 
+console.log(productManager.getProductById(4)); // Esto mostrará "No encontrado"
